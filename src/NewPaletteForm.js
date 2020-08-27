59a323dc393ec1ab83002fad36dff9b2390e2a28
Hadi Naamani <hadi2000naamani@gmail.com>
2020-08-27 19:45:01 +0300

    Add randomColor and clearPalet

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -83,13 +83,16 @@ const styles = theme => ({
 
 
 class NewPaletteForm extends Component {
+    static defaultProps = {
+        maxColors: 20
+    }
     constructor(props) {
         super(props);
         this.state = {
             open: true,
             currentColor: "teal",
             newColorName: "",
-            colors: [{ color: "blue", name: "blue" }],
+            colors: this.props.palettes[0].colors,
             newPaletteName: ""
         };
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
@@ -97,6 +100,8 @@ class NewPaletteForm extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.removeColor = this.removeColor.bind(this);
+        this.clearColors = this.clearColors.bind(this);
+        this.addRandomColor = this.addRandomColor.bind(this);
     }
 
     componentDidMount() {
@@ -135,6 +140,15 @@ class NewPaletteForm extends Component {
             [e.target.name]: e.target.value
         })
     }
+    clearColors() {
+        this.setState({ colors: [] });
+    }
+    addRandomColor() {
+        const allColors = this.props.palettes.map(p => p.colors).flat();
+        var rand = Math.floor(Math.random() * allColors.length);
+        const randomColor = allColors[rand];
+        this.setState({ colors: [...this.state.colors, randomColor] });
+    }
     handleSubmit() {
         let newName = this.state.newPaletteName;
         const newPalette = { paletteName: newName, id: newName.toLowerCase().replace(/ /g, '-'), colors: this.state.colors };
@@ -152,8 +166,9 @@ class NewPaletteForm extends Component {
         }));
     };
     render() {
-        const { classes } = this.props;
-        const { open } = this.state;
+        const { classes, maxColors } = this.props;
+        const { open, colors } = this.state;
+        const paletteIsFull = colors.length >= maxColors;
 
         return (
             <div className={classes.root} >
@@ -203,14 +218,18 @@ class NewPaletteForm extends Component {
                         Design Your Palette
                     </Typography>
                     <div>
-                        <Button variant="contained" color="secondary">Clear Palette</Button>
-                        <Button variant="contained" color="primary">Random Color</Button>
+                        <Button variant="contained" color="secondary" onClick={this.clearColors}>
+                            Clear Palette
+                        </Button>
+                        <Button disabled={paletteIsFull} variant="contained" color="primary" onClick={this.addRandomColor}>
+                            Random Color
+                        </Button>
                     </div>
                     <ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor} />
                     <ValidatorForm onSubmit={this.addNewColor}>
                         <TextValidator name="newColorName" value={this.state.newColorName} onChange={this.handleChange} validators={['required', 'isColorNameUnique', 'isColorUnique']} errorMessages={['Enter a color name', 'Color name must be unique', 'Color already used']} />
-                        <Button type="submit" variant="contained" color="primary" style={{ backgroundColor: this.state.currentColor }} >
-                            Add Color
+                        <Button disabled={paletteIsFull} type="submit" variant="contained" color="primary" style={{ backgroundColor: paletteIsFull? "grey": this.state.currentColor }} >
+                            {paletteIsFull ? "Palette Full" : "Add Color"}
                         </Button>
                     </ValidatorForm>
 
@@ -223,7 +242,7 @@ class NewPaletteForm extends Component {
                     <div className={classes.drawerHeader} />
 
                     <DraggableColorList
-                        colors={this.state.colors}
+                        colors={colors}
                         removeColor={this.removeColor}
                         axis='xy'
                         onSortEnd={this.onSortEnd}
