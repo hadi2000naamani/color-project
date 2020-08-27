63398e639b30163a8dd77c0f7b5c3c49244d8ed4
Hadi Naamani <hadi2000naamani@gmail.com>
2020-08-27 18:45:04 +0300

    Add delete to PaletteForm

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {withStyles} from "@material-ui/styles";
+import { withStyles } from "@material-ui/styles";
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const styles = {
-    root:{
+    root: {
         width: '20%',
         height: '25%',
         margin: '0 auto',
@@ -11,12 +11,12 @@ const styles = {
         position: 'relative',
         cursor: 'pointer',
         marginBottom: '-3.5px',
-        "&:hover svg":{
-            color:'white',
-            transform:'scale(1.5)'
+        "&:hover svg": {
+            color: 'white',
+            transform: 'scale(1.5)'
         }
     },
-    boxContent:{
+    boxContent: {
         position: 'absolute',
         width: '100%',
         left: '0px',
@@ -26,23 +26,23 @@ const styles = {
         letterSpacing: '1px',
         textTransform: 'uppercase',
         fontSize: '12px',
-        display:'flex',
-        justifyContent:'space-between'
+        display: 'flex',
+        justifyContent: 'space-between'
     },
-    deleteIcon:{
-        transition:'all 0.3s ease-in-out'
+    deleteIcon: {
+        transition: 'all 0.3s ease-in-out'
     }
 }
 
 function DraggableColorBox(props) {
-    const {classes} = props
+    const { classes, handleClick, name, color } = props
     return (
-        <div className={classes.root} style={{ background: props.color }}>
-            <div className= {classes.boxContent}>
-                <span>{props.name}</span>
-                <DeleteIcon className={classes.deleteIcon} />
+        <div className={classes.root} style={{ background:color }}>
+            <div className={classes.boxContent}>
+                <span>{name}</span>
+                <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
             </div>
-           
+
         </div>
     )
 }
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -139,6 +139,11 @@ class NewPaletteForm extends Component {
         this.props.savePalette(newPalette);
         this.props.history.push("/");
     }
+    removeColor(colorName){
+        this.setState({
+            colors:this.state.colors.filter(color => color.name !== colorName)
+        })
+    }
     render() {
         const { classes } = this.props;
         const { open } = this.state;
@@ -211,7 +216,7 @@ class NewPaletteForm extends Component {
                     <div className={classes.drawerHeader} />
 
                     {this.state.colors.map(color => (
-                        <DraggableColorBox color={color.color} name={color.name} />
+                        <DraggableColorBox key={color.name} handleClick={() => this.removeColor(color.name)} color={color.color} name={color.name} />
                     ))}
 
                 </main>
